451e8274eef855030c594a5b4e7eb8d96dcbb15b
Yannick Longval <yannicklongval2014@gmail.com>
2022-02-19 20:18:35 -0500

    GUI
    wah

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ pygame.display.set_caption('Student.me')
 #background colour
 BACKGROUND = (40, 45, 50)
 
-TEXT_FONT = pygame.font.SysFont('comicsans', 80)
-SMALL_FONT = pygame.font.SysFont('comicsans', 40)
+TEXT_FONT = pygame.font.SysFont('arial', 60)
+MEDIUM_FONT = pygame.font.SysFont('arial', 40)
+SMALL_FONT = pygame.font.SysFont('arial', 30)
 
 #drawing game screen
 def draw_window(state, student):
@@ -21,33 +22,33 @@ def draw_window(state, student):
     pygame.display.update()
 
 def game_screen(student):
-    prompt = prompts["tests"][0]
-    prompt_text = TEXT_FONT.render(prompt.name, 1, (0, 0, 0))
-    prompt_rect = prompt_text.get_rect()
-    prompt_rect.center = (WIDTH // 2, HEIGHT // 2)
-    pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(WIDTH // 4 - 100, HEIGHT // 4 + 100, 500, 200))
-    #WIN.blit(prompt_text, prompt_rect)
-    display_text(TEXT_FONT, ["TEST", "TEXT"], (WIDTH // 2, HEIGHT // 2))
-    pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(WIDTH // 2 - 250, HEIGHT - 200, 200, 100))
-    pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(WIDTH - 250, HEIGHT - 200, 200, 100))
-    WIN.blit(TEXT_FONT.render(prompt.opt1.name, 1, (0, 0, 0)), (WIDTH - 220, HEIGHT - 170))
-    WIN.blit(TEXT_FONT.render(prompt.opt2.name, 1, (0, 0, 0)), (WIDTH // 2 - 190, HEIGHT - 170))
+    prompt = prompts["encounter"][2]
+    pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(WIDTH // 4 - 100, HEIGHT // 4 + 50, 500, 200))
+    display_text(TEXT_FONT, prompt.name, (WIDTH // 2, HEIGHT // 2), (0, 0, 0))
 
-    WIN.blit(SMALL_FONT.render("HEALTH:", 1, (255, 255, 255)), (30, 30))
+    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 260, HEIGHT - 260, 220, 100))
+    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH - 260, HEIGHT - 260, 220, 100))
+    display_text(MEDIUM_FONT, prompt.opt1.name, (WIDTH // 2 - 150, HEIGHT - 190), (255, 255, 255))
+    display_text(MEDIUM_FONT, prompt.opt2.name, (WIDTH - 150, HEIGHT - 190), (255, 255, 255))
+
+    WIN.blit(SMALL_FONT.render("HEALTH:", 1, (255, 255, 255)), (40, 25))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 30, 350, 30))
     pygame.draw.rect(WIN, (0, 255, 0), pygame.Rect(220, 30, (350 * (student.health / student.maxes["health"])) // 1, 30))
 
-    WIN.blit(SMALL_FONT.render("STRESS:", 1, (255, 255, 255)), (30, 80))
+    WIN.blit(SMALL_FONT.render("STRESS:", 1, (255, 255, 255)), (40, 75))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 80, 350, 30))
     pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(220, 80, (350 * (student.stress / student.maxes["stress"])) // 1, 30))
 
-    WIN.blit(SMALL_FONT.render("SMARTS:", 1, (255, 255, 255)), (30, 130))
+    WIN.blit(SMALL_FONT.render("SMARTS:", 1, (255, 255, 255)), (40, 125))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 130, 350, 30))
     pygame.draw.rect(WIN, (0, 0, 255), pygame.Rect(220, 130, (350 * (student.smarts / student.maxes["smarts"])) // 1, 30))
 
-    WIN.blit(SMALL_FONT.render("HAPPINESS:", 1, (255, 255, 255)), (30, 180))
+    WIN.blit(SMALL_FONT.render("HAPPINESS:", 1, (255, 255, 255)), (40, 175))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 180, 350, 30))
     pygame.draw.rect(WIN, (255, 255, 0), pygame.Rect(220, 180, (350 * (student.happiness / student.maxes["happiness"])) // 1, 30))
+
+    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH//2 - 50, HEIGHT - 90, 100, 50))
+    WIN.blit(SMALL_FONT.render("PAUSE", 1, (255, 255, 255)), (WIDTH // 2 - 40, HEIGHT - 85))
 
 #run game
 def main():
@@ -69,9 +70,9 @@ def main():
             mouse = pygame.mouse.get_pos()
             draw_window(state, student)
 
-def display_text(font, phrase, center):
+def display_text(font, phrase, center, color):
     for index, chunk in enumerate(phrase):
-        text = font.render(chunk, 1, (0, 0, 0))
+        text = font.render(chunk, 1, color)
         rect = text.get_rect()
         rect.center = (center[0], (center[1] - (rect.height * len(phrase)//2) + (rect.height) * index))
         WIN.blit(text, rect)
