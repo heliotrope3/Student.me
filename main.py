fcd34ae6a61bd67c2d48c0dc6fd19a38fff33718
heliotrope3 <elianaqyliu@gmail.com>
2022-02-19 21:53:28 -0500

    i changed the colours and made them pretty

--- main.py
+++ main.py
@@ -15,26 +15,26 @@ MEDIUM_FONT = pygame.font.SysFont('arial', 40)
 SMALL_FONT = pygame.font.SysFont('arial', 30)
 
 def draw_menu_screen():
-    WIN.fill(BACKGROUND)
-    WIN.blit(TEXT_FONT.render("STUDENT.me", 1, (255, 255, 255)), (WIDTH // 2 - 150, 200))
-    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 75, HEIGHT // 2 - 20, 150, 70))
-    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 2 - 70, HEIGHT // 2 - 15, 140, 60))
-    WIN.blit(MEDIUM_FONT.render("START", 1, (255, 255, 255)), (WIDTH // 2 - 50, HEIGHT // 2 - 10))
+    WIN.fill((237, 255, 238))
+    WIN.blit(TEXT_FONT.render("STUDENT.me", 1, (0, 0, 0)), (WIDTH // 2 - 150, 200))
+    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 2 - 75, HEIGHT // 2 - 20, 150, 70))
+    pygame.draw.rect(WIN, (237, 255, 238), pygame.Rect(WIDTH // 2 - 70, HEIGHT // 2 - 15, 140, 60))
+    WIN.blit(MEDIUM_FONT.render("START", 1, (0, 0, 0)), (WIDTH // 2 - 50, HEIGHT // 2 - 10))
     pygame.display.update()
 
 def draw_select_screen():
-    WIN.fill(BACKGROUND)
-    WIN.blit(TEXT_FONT.render("Choose your level", 1, (255, 255, 255)), (WIDTH // 2 - 195, 200))
-    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 200, HEIGHT // 2 - 20, 400, 70))
-    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 2 - 195, HEIGHT // 2 - 15, 390, 60))
-    WIN.blit(MEDIUM_FONT.render("High School (Regular)", 1, (255, 255, 255)), (WIDTH // 2 - 160, HEIGHT // 2 - 10))
-    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 200, HEIGHT // 2 + 100, 400, 70))
-    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 2 - 195, HEIGHT // 2 + 105, 390, 60))
-    WIN.blit(MEDIUM_FONT.render("University (Hard)", 1, (255, 255, 255)), (WIDTH // 2 - 115, HEIGHT // 2 + 110))
+    WIN.fill((255, 232, 238))
+    WIN.blit(TEXT_FONT.render("Choose your level", 1, (0, 0, 0)), (WIDTH // 2 - 195, 200))
+    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 2 - 200, HEIGHT // 2 - 20, 400, 70))
+    pygame.draw.rect(WIN, (242, 237, 255), pygame.Rect(WIDTH // 2 - 195, HEIGHT // 2 - 15, 390, 60))
+    WIN.blit(MEDIUM_FONT.render("High School (Regular)", 1, (0, 0, 0)), (WIDTH // 2 - 160, HEIGHT // 2 - 10))
+    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 2 - 200, HEIGHT // 2 + 100, 400, 70))
+    pygame.draw.rect(WIN, (230, 239, 255), pygame.Rect(WIDTH // 2 - 195, HEIGHT // 2 + 105, 390, 60))
+    WIN.blit(MEDIUM_FONT.render("University (Hard)", 1, (0, 0, 0)), (WIDTH // 2 - 115, HEIGHT // 2 + 110))
     pygame.display.update() 
 
 def draw_pause_screen(student):
-    WIN.fill(BACKGROUND)
+    WIN.fill((237, 255, 238))
     pygame.display.update()
 
 def draw_end_screen(student):
@@ -42,44 +42,44 @@ def draw_end_screen(student):
     pygame.display.update()
 
 def draw_game_screen(student):
-    WIN.fill(BACKGROUND)
+    WIN.fill((242, 237, 255))
     prompt = prompts["encounter"][2]
     pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 4 - 105, HEIGHT // 4 + 45, 510, 210))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(WIDTH // 4 - 100, HEIGHT // 4 + 50, 500, 200))
     display_text(TEXT_FONT, prompt.name, (WIDTH // 2, HEIGHT // 2), (0, 0, 0))
 
     pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH // 2 - 260, HEIGHT - 260, 220, 100))
-    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 255, HEIGHT - 255, 210, 90))
+    pygame.draw.rect(WIN, (237, 255, 238), pygame.Rect(WIDTH // 2 - 255, HEIGHT - 255, 210, 90))
     pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH - 260, HEIGHT - 260, 220, 100))
-    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH - 255, HEIGHT - 255, 210, 90))
-    display_text(MEDIUM_FONT, prompt.opt1.name, (WIDTH // 2 - 150, HEIGHT - 190), (255, 255, 255))
-    display_text(MEDIUM_FONT, prompt.opt2.name, (WIDTH - 150, HEIGHT - 190), (255, 255, 255))
+    pygame.draw.rect(WIN, (237, 255, 238), pygame.Rect(WIDTH - 255, HEIGHT - 255, 210, 90))
+    display_text(MEDIUM_FONT, prompt.opt1.name, (WIDTH // 2 - 150, HEIGHT - 190), (0, 0, 0))
+    display_text(MEDIUM_FONT, prompt.opt2.name, (WIDTH - 150, HEIGHT - 190), (0, 0, 0))
 
-    WIN.blit(SMALL_FONT.render("HEALTH:", 1, (255, 255, 255)), (40, 25))
+    WIN.blit(SMALL_FONT.render("HEALTH:", 1, (0, 0, 0)), (40, 25))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 30, 350, 30))
-    pygame.draw.rect(WIN, (0, 255, 0), pygame.Rect(220, 30, (350 * (student.health / student.maxes["health"])) // 1, 30))
+    pygame.draw.rect(WIN, (149, 255, 112), pygame.Rect(220, 30, (350 * (student.health / student.maxes["health"])) // 1, 30))
 
-    WIN.blit(SMALL_FONT.render("STRESS:", 1, (255, 255, 255)), (40, 75))
+    WIN.blit(SMALL_FONT.render("STRESS:", 1, (0, 0, 0)), (40, 75))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 80, 350, 30))
-    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(220, 80, (350 * (student.stress / student.maxes["stress"])) // 1, 30))
+    pygame.draw.rect(WIN, (255, 120, 120), pygame.Rect(220, 80, (350 * (student.stress / student.maxes["stress"])) // 1, 30))
 
-    WIN.blit(SMALL_FONT.render("SMARTS:", 1, (255, 255, 255)), (40, 125))
+    WIN.blit(SMALL_FONT.render("SMARTS:", 1, (0, 0, 0)), (40, 125))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 130, 350, 30))
-    pygame.draw.rect(WIN, (0, 0, 255), pygame.Rect(220, 130, (350 * (student.smarts / student.maxes["smarts"])) // 1, 30))
+    pygame.draw.rect(WIN, (120, 125, 255), pygame.Rect(220, 130, (350 * (student.smarts / student.maxes["smarts"])) // 1, 30))
 
-    WIN.blit(SMALL_FONT.render("HAPPINESS:", 1, (255, 255, 255)), (40, 175))
+    WIN.blit(SMALL_FONT.render("HAPPINESS:", 1, (0, 0, 0)), (40, 175))
     pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(220, 180, 350, 30))
-    pygame.draw.rect(WIN, (255, 255, 0), pygame.Rect(220, 180, (350 * (student.happiness / student.maxes["happiness"])) // 1, 30))
+    pygame.draw.rect(WIN, (255, 242, 120), pygame.Rect(220, 180, (350 * (student.happiness / student.maxes["happiness"])) // 1, 30))
 
-    pygame.draw.rect(WIN, (255, 255, 255), pygame.Rect(WIDTH//2 - 55, HEIGHT - 95, 110, 60))
-    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH//2 - 50, HEIGHT - 90, 100, 50))
-    WIN.blit(SMALL_FONT.render("PAUSE", 1, (255, 255, 255)), (WIDTH // 2 - 40, HEIGHT - 85))
+    pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(WIDTH//2 - 55, HEIGHT - 95, 110, 60))
+    pygame.draw.rect(WIN, (254, 255, 232), pygame.Rect(WIDTH//2 - 50, HEIGHT - 90, 100, 50))
+    WIN.blit(SMALL_FONT.render("PAUSE", 1, (0, 0, 0)), (WIDTH // 2 - 40, HEIGHT - 85))
     pygame.display.update()
 
 #run game
 def main():
     student = Student("uni")
-    state = 'play'
+    state = 'select'
     run = True
     while run:
 
