045b5f13fbd6b81a7987403c26ca068d17da03a2
heliotrope3 <elianaqyliu@gmail.com>
2022-02-19 19:36:18 -0500

    idk
    idk

--- Task.py
+++ Task.py
@@ -1,5 +1,9 @@
 class Task:
 
-    def __init__(self, name, stress):
+    def __init__(self, name, health, stress, smarts, happiness):
         self.name = name
+        self.health = health
         self.stress = stress
+        self.smarts = smarts
+        self.happiness = happiness
+        
